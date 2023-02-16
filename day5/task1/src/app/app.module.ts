2c1cc131e1e13fc16c5411f480d1e59ebb1e7eb7
Viplav P <78026984+viplav-dev@users.noreply.github.com>
2023-02-16 15:05:39 +0530

    first commit

---
+++ day5/task1/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import {HttpClientModule} from '@angular/common/http'
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
